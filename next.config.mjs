9c1b851f57dd91dea3b929d6f28b013f18d6358e
Ananya <ananya200696@gmail.com>
2025-09-25 16:48:59 +0530

    basepath,assetpath removed

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,8 @@
 const nextConfig = {
   // output: "export", // 👈 tells Next to create a static export during build
   output: "export",
-  basePath: "/myapp",
-  assetPrefix: "/myapp/",
+  // basePath: "/myapp",
+  // assetPrefix: "/myapp/",
 };
 
 export default nextConfig;
